3b3335ad6907defb57535a9be86269beff142bdb
Juan Carlos Cornejo <cornejo@jlab.org>
2017-01-30 13:21:39 -0500

    Implemented ability to determine next available SetID.  So I don't have to remember

--- date_kin_set.sh
+++ date_kin_set.sh
@@ -2,10 +2,16 @@
 
 source date_kin.sh
 
+findSetID=0
 if [ -z $3 ]; then
   echo "SetID empty"
   exit
+elif [ $3 == "next" ]; then
+  echo "The next SetID will be determined for each kinematic"
+  findSetID=1
+  setID=0
+else
+  setID=$(printf "%03d" $3)
 fi
 
-setID=$(printf "%03d" $3)
 
--- saveresults.sh
+++ saveresults.sh
@@ -10,11 +10,23 @@ for (( i=0; i<${#kinlist}; i++ )); do
     continue ## Skip bad kin number
   fi
   kin=$(printf "%02d" $kin)
-  echo $kin
-  ./copy_result2db.sh ${dateCmd} ${kin} ${setID}
+  csvFilePrefix="results/db/${dateCmd}_kin${kin}_"
+
+  ## Find the next setID
+  if [ $findSetID == 1 ]; then
+    ## Get a list of the db files for this kinematic, reverse sorted
+    ## which puts the last set (by number) ontop
+    thisKinFiles=(`ls -r ${csvFilePrefix}[0-9][0-9][0-9].csv`)
+    ## Then pick the first entry and pickout the setID number
+    lastSetID=`echo ${thisKinFiles[0]} | grep -oP "${csvFilePrefix}\K[0-9][0-9][0-9]"`
+    setID=$(printf "%03d" $((lastSetID+1)))
+    echo "Found Last SetID: ${lastSetID}. New SetID: ${setID}"
+  fi
+  #echo $kin
+  #./copy_result2db.sh ${dateCmd} ${kin} ${setID}
   inFile="results/${dateCmd}/kin${kin}/kin${kin}_results.csv"
   inExtraCSVFile="scripts/${dateCmd}/comment_gmn_kin${kin}.csv"
-  csvFile="results/db/${dateCmd}_kin${kin}_${setID}.csv"
+  csvFile="${csvFilePrefix}${setID}.csv"
 
 
   echo "Making $csvFile"
